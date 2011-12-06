e0ba291fca49438be4f84d9db5ae647f04a8a366
Phillip Garland <pgarland@gmail.com>
2011-12-06 00:33:10 -0800

    Upgrade clojure and noir dependencies to 1.3 and 1.2.1, respectively

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject noir-blog "0.1.0"
             :description "A fully functional blog that serves an example of a noir project."
-            :dependencies [[org.clojure/clojure "1.2.1"]
+            :dependencies [[org.clojure/clojure "1.3.0"]
                            [clj-time "0.3.0"]
-                           [noir "1.0.0"]
+                           [noir "1.2.1"]
                            [org.markdownj/markdownj "0.3.0-1.0.2b4"]
                            [simpledb "0.1.4"]]
             :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
