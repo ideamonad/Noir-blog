55350269d3dff32caf14f558830d0952d5594768
Phillip Garland <pgarland@gmail.com>
2011-12-12 21:20:25 -0800

    tags->string: new function

--- src/noir_blog/models/tag.clj
+++ src/noir_blog/models/tag.clj
@@ -1,5 +1,6 @@
 (ns noir-blog.models.tag
   (:require [simpledb.core :as db]
+            [clojure.string :as string]
             [noir.validation :as vali]))
 
 (defn init! []
@@ -8,3 +9,6 @@
 (defn tag-update-fn [existing-tags new-tags id]
   (let [new-tags-map (reduce merge (for [tag new-tags] {tag [id]}))]
     (merge-with into existing-tags new-tags-map)))
+
+(defn tags->string [tags]
+  (string/join ", " tags))
