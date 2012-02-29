1d397f441143edd7644533f2a99f8e4878a819c0
Phillip Garland <pgarland@gmail.com>
2012-02-28 21:10:15 -0800

    Add dep on Java Rome lib for RSS/Atom

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
                            [clj-time "0.3.0"]
                            [noir "1.2.1"]
                            [org.markdownj/markdownj "0.3.0-1.0.2b4"]
-                           [simpledb "0.1.4"]]
+                           [simpledb "0.1.4"]
+                           [rome "1.0"]]
             :dev-dependencies [[clojure-source "1.3.0"]]
             :main noir-blog.server)
 
