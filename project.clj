ed2cd23f27a64472a454ef8cc424c7de810f1d69
Phillip Garland <pgarland@gmail.com>
2012-04-17 13:27:19 -0700

    Upgrade clojure requirement to 1.4

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject noir-blog "0.1.0"
             :description "A fully functional blog that serves an example of a noir project."
-            :dependencies [[org.clojure/clojure "1.3.0"]
+            :dependencies [[org.clojure/clojure "1.4.0"]
                            [clj-time "0.3.7"]
                            [noir "1.3.0-beta2"]
                            [org.markdownj/markdownj "0.3.0-1.0.2b4"]
