64581921c40bca2b36599e4f1169a30d1e0ca66c
Phillip Garland <pgarland@gmail.com>
2012-04-17 14:25:48 -0700

    Upgrade clojure source dev dependency to 1.4

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
                            [org.markdownj/markdownj "0.3.0-1.0.2b4"]
                            [simpledb "0.1.4"]
                            [rome "1.0"]]
-            :dev-dependencies [[clojure-source "1.3.0"]]
+            :dev-dependencies [[clojure-source "1.4.0"]]
             :main noir-blog.server)
 
