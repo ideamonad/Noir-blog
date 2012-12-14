8d5cf973e1b8c17ec9dff7873b0cc2ea554317f1
Phillip Garland <pgarland@gmail.com>
2012-12-13 21:40:42 -0800

    Update to latest (and last) Noir) and change profiles

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject noir-blog/noir-blog "0.1.0" 
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-time "0.3.7"]
-                 [noir "1.3.0-beta2"]
+                 [noir "1.3.0"]
                  [org.markdownj/markdownj "0.3.0-1.0.2b4"]
                  [simpledb "0.1.4"]
                  [rome "1.0"]]
-  :profiles {:dev {}}
+  :profiles {:user {}}
   :main noir-blog.server
   :min-lein-version "2.0.0"
   :description "A fully functional blog that serves an example of a noir project.")
