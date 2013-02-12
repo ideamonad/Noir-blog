38f517f060f01c4953018acf8b5e0ba9ede16f4e
Phillip Garland <pgarland@gmail.com>
2013-02-11 20:17:52 -0800

    Update to newest version of ring to maybe fix session management with chrome

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject noir-blog/noir-blog "0.1.0" 
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]            
                  [clj-time "0.3.7"]
+                 [ring "1.1.8"]
                  [noir "1.3.0"]
                  [org.markdownj/markdownj "0.3.0-1.0.2b4"]
                  [simpledb "0.1.4"]
